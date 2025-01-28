0ff0622f643d80abd1ab642de9ecbf253baa3b29
ashraidiam <ash.hammond@raidiam.com>
2025-01-28 20:52:35 +0000

    update main

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "6.5.0"
+    }
+  }
+}
+
+
+provider "github" {
+  owner = "RaidiamDev"
+  app_auth {}
+}
+
+
+resource "github_repository" "example" {
+  name                 = "example"
+  description          = "My awesome codebase"
+  vulnerability_alerts = true
+  visibility           = "public"
+}
+
+resource "github_repository" "github" {
+  name          = "github"
+  description   = "Github provisioner repo"
+  has_downloads = true
+  has_issues    = true
+  has_projects  = true
+
+  visibility = "public"
+}
